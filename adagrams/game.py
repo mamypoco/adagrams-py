de82bc14c5ac611d1d85d66e25b07ae4983b745d
mami <mamypoco@gmail.com>
2025-09-11 14:54:21 -0700

    first commit

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,44 @@
 from random import randint
 
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    
+    #Returns an array of ten strings
+    hand = [] #["A", "K", "S", "O", "R", "E", "T", "A", "I", "N", "L"]
+    # for key, value in LETTER_POOL.items():
+    #     LETTER_POOL[key]
+
+    letter = randint(list(LETTER_POOL.keys()))
+    print(letter)
+
 
 def uses_available_letters(word, letter_bank):
     pass
